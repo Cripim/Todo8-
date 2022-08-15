10e174375c93bab8f6092ffe5249f5534cce2077
Diego Tavares <mello.gredy@gmail.com>
2022-08-15 13:26:31 -0300

    DB

---
+++ data/db.js
@@ -0,0 +1,16 @@
+const db = {
+  tips: [
+    "Faça atividade física",
+    "Alimente-se bem",
+    "Beba água",
+    "Revise conteúdos",
+    "Faça intervalos e descanse",
+    "Organize sua agenda",
+    "Não vire a noite programando",
+    "Leia",
+    "Estude com qualidade, não quantidade",
+    "Pratique por 1 hora todos os dias",
+  ],
+};
+
+export default db;
