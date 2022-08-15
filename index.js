11ea84e0ebd08e94ffd6f2e60a378af6dd2ba953
Diego Tavares <mello.gredy@gmail.com>
2022-08-15 13:18:13 -0300

    Estruturando o index principal

---
+++ index.js
@@ -0,0 +1,12 @@
+import Express from "express";
+import db from "./data/db.js";
+import tips from "./controllers/controller.js";
+
+const server = Express();
+server.use(Express.json());
+
+tips(server, db);
+
+server.listen(4000, () => {
+  console.log("Servidor ativo em: http://localhost:4000");
+});
